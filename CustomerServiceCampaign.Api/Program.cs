64be4f8a9ccc79e0fb725e3d503633b1eebdd728
Jovan Aleksić <jovan.aleksic@netica.si>
2024-01-27 23:50:59 +0100

    Initial commit

---
+++ CustomerServiceCampaign.Api/Program.cs
@@ -0,0 +1,36 @@
+using CustomerServiceCampaign.Api;
+using LoggerService;
+using Serilog;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+// Add services to the container.
+builder.Services.AddCors();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration).CreateLogger();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
